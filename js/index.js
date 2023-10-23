edad02cc78f2aead421d2da8d6e60bc6ea01f8cd
pain-despair <slavikrymar08@gmail.com>
2023-10-23 15:19:34 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function generateKey(length, characters) {
+    let result = ''
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+}
+let key = generateKey(7, sawrogfiha3)
+console.log(key);
